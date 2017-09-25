42d833e24d740b89c9be77be51dae0081679552e
4hiziri <meirvg@gmail.com>
2017-09-25 14:36:01 +0900

    Add downloader for parsing error of fast-http

--- handmade-server.lisp
+++ handmade-server.lisp
@@ -1,18 +1,57 @@
-(ql:quickload '(:ningle :clack :lack :fast-http :split-sequence :flexi-streams :cl-fad))
+(ql:quickload '(:ningle :clack :lack :fast-http :flexi-streams :cl-fad :split-sequence))
 
 (defvar *app* (make-instance 'ningle:<app>))
 (defvar *port* 30018)
 (defvar *download-dir* "~/Downloads/")
 
-(defun parse-multipart/form-data (data)
-  (let ((data (split-sequence #\; data))
-	(alist nil))
-    (push (cons :content-disposition (second (split-sequence #\: (pop data)))) alist)
-    (push (cons :name (second (split-sequence #\= (pop data)))) alist)
-    (push (cons :filename data) alist)
-    alist))
+(use-package :split-sequence)
 
-(defun download-file (filename stream &optional (directory *download-dir*))  
+(defun split-seq-by-seq (seq1 seq2)
+  "(split-seq-by-seq '(1 2) '(1 2 0 3 1 2 4 5 7 1 2)) => ((1 2) (0 3 1 2) (4 5 7 1 2) NIL)"
+  (labels ((inner-loop (seq2 acc)
+	     (let ((pos (search seq1 seq2)))
+	       (if pos
+		   (inner-loop (subseq seq2 (+ pos (length seq1)))
+			       (cons (subseq seq2 0 (+ pos (length seq1))) acc))
+		   (reverse (cons seq2 acc))))))
+    (inner-loop seq2 nil)))
+
+(defun split-field-multipart (content)
+  (flet ((inner-convert (pair)
+	   (cons (map 'string #'code-char (first pair))
+		 (second pair))))
+    (let* ((fields (split-seq-by-seq '(#xd #xa) content))
+	   (disposition (nth 1 fields))
+	   (type (nth 2 fields))
+	   (content (apply #'append (mapcar (lambda (x) (coerce x 'list))
+					    (subseq fields 4 (- (length fields) 2))))))
+      (list
+       (inner-convert (split-sequence (char-code #\:) disposition))
+       (inner-convert (split-sequence (char-code #\:) type))
+       (cons "content" (subseq content 0 (- (length content) 2)))))))
+
+(defun split-disposition (disposition)  
+  (let* ((disposition (map 'string #'code-char disposition))
+	 (fields (split-sequence #\; disposition)))
+    (mapcar (lambda (x)
+	      (let ((list (split-sequence #\= x)))
+		(cons (remove #\space (first list))
+		      (remove-if (lambda (x) (or (char= x (code-char #xa))
+						 (char= x (code-char #xd))
+						 (char= x #\")
+						 (char= x #\')))
+				 (second list)))))
+	    fields)))
+
+(defun parse-multipart (content)
+  (let* ((alist (split-field-multipart content))
+	 (filename (cdr (assoc "filename"
+			       (split-disposition (cdr (assoc "Content-Disposition" alist :test #'string=)))
+			       :test #'string=)))
+	 (content (cdr (assoc "content" alist :test #'string=))))
+    (cons filename content)))
+
+(defun download-from-stream (filename stream &optional (directory *download-dir*))  
   (if (or (fad:directory-exists-p directory)
 	  (nth-value 1 (ensure-directories-exist directory)))
       (with-open-file (file (fad:merge-pathnames-as-file directory filename)
@@ -20,6 +59,18 @@
 			    :element-type 'unsigned-byte
 			    :if-exists :rename)
 	(do ((byte (read-byte stream nil nil) (read-byte stream nil nil)))
+	    ((null byte) t)
+	  (write-byte byte file)))
+      nil))
+
+(defun download-from-list (filename list &optional (directory *download-dir*))  
+  (if (or (fad:directory-exists-p directory)
+	  (nth-value 1 (ensure-directories-exist directory)))
+      (with-open-file (file (fad:merge-pathnames-as-file directory filename)
+			    :direction :output
+			    :element-type 'unsigned-byte
+			    :if-exists :rename)
+	(do ((byte (pop list) (pop list)))
 	    ((null byte) t)
 	  (write-byte byte file)))
       nil))
@@ -35,13 +86,22 @@
 <input type='submit' value='send'>
 </form>")
 
+(defun ningle-parse (params)
+  (let ((stream (second (car params)))
+	(field (third (car params))))
+    (and stream
+	 (download-from-stream (gethash "filename" field)
+			       (flex:make-flexi-stream (second (car params)))))))
+
+(defun hack-parse (content)
+  (let ((alist (parse-multipart content)))
+    (download-from-list (first alist) (coerce (rest alist) 'list))))
+
 (setf (ningle:route *app* "/send/upload" :method :post)
       #'(lambda (params)
-	  (let ((stream (second (car params)))
-		(field (third (car params))))
-	    (if (and stream (download-file (gethash "filename" field)
-					   (flex:make-flexi-stream (second (car params)))
-					   "/home/tkgsy/misc/project/handmade-server/src/download/"))
+	  (let ((*download-dir* "~/misc/project/handmade-server/download/"))
+	    (if (or (ningle-parse params)
+		    (hack-parse (lack.request:request-content ningle:*request*)))
 		"Success!"
 		"Fail!"))))
 
